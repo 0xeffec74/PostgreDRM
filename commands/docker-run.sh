30a0d36486667dd6053b77f823037b9191beabfd
Shogo Noge <s.noge@effect-effect.com>
2021-08-12 17:12:22 +0900

    make docker-compose

--- commands/docker-run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# docker run
-# -------------------------------
-docker run --name some-postgis \
--e POSTGRES_PASSWORD=mysecretpassword \
--e DISPLAY=${DISPLAY}  --mount type=bind,src=/tmp/.X11-unix/,dst=/tmp/.X11-unix \
--d postgis-qgis
